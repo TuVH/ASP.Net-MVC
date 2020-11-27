d6f537ec8866157641bd24158c52ea245ee7b11c
Tu <hoangtu1392001@gmail.com>
2020-11-27 21:46:08 +0700

    [Add]_ASP.NET (END)

---
+++ WebApplication/OnlineShop/Controllers/SanPhamController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShop.Models;
+namespace OnlineShop.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: SanPham
+        public ActionResult SanPham1()
+        {
+
+            return View();
+        }
+        public ActionResult SanPham2()
+        {
+
+            return View();
+        }
+        //Tạo patial view
+        public ActionResult SanPhamPartial()
+        {
+
+            return PartialView();
+        }
+    }
+}
